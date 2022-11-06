a04138e48ecb69bae37600fa3b5b49ebf71eeaeb
ardee <ardee@10.239.89.44>
2022-11-06 16:08:05 -0700

    Removed ConnectionPool class and updated the DBUtil for JPA

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -1,29 +1,16 @@
 package dataaccess;
-
-import java.sql.*;
-
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 /**
  *
  * @author ardee
  */
+
 public class DBUtil {
-    public static void closePreparedStatement (Statement ps) {
-        try {
-            if (ps != null) {
-                ps.close();
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-    }
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("UserPU");
     
-    public static void closeResultSet (ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
     }
 }
